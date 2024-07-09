ee2bf2506a0881f4db8d72b70e3f7c2d15bcb4f1
= <=>
2024-07-09 22:13:19 +0300

    version 2

--- src/Main.java
+++ src/Main.java
@@ -28,5 +28,37 @@ public class Main {
             }
             System.out.println();
         }
+        System.out.println();
+        System.out.println();
+
+
+        // yıldızlardan f yazdıralım 0. ve 2. satır ve 0.stun yıldız olcak
+        int[][] f = new int[5][5];
+        for (int row = 0; row < f.length; row++) {
+            for (int col = 0; col < f[0].length; col++) {
+                if (row == 0) {
+                    f[row][col] = 1;
+                } else if (row == 2) {
+                    f[row][col] = 1;
+                }
+
+                if (col==0) {
+                    f[row][col]=1;
+                }
+            }
+        }
+        for (int[] row:f){
+            for(int col:row){
+               switch (col){
+                   case 0:
+                       System.out.print(" ");
+                       break;
+                   case 1:
+                       System.out.print("*");
+                       break;
+               }
+            }
+            System.out.println();
+        }
     }
 }
